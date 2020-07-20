c5f3b9c577cf40d485d38f40fb9f663e8450dd18
Gang Zhang <gang.z@cern.ch>
2020-07-20 14:20:18 +0200

    set correct environment

--- setUpEnvironment.sh
+++ setUpEnvironment.sh
@@ -6,10 +6,10 @@ export LD_LIBRARY_PATH="${FASERTRACKER_DIR}/faserTracker_run/lib64:$LD_LIBRARY_P
 
 ## TODO: allow for more general options instead of hardcoding lxplus setup
 ## lxplus setup
-#platform=x86_64-slc6-gcc62-opt
-#view=/cvmfs/sft.cern.ch/lcg/views/LCG_90a/${platform}
+platform=x86_64-slc6-gcc62-opt
+view=/cvmfs/sft.cern.ch/lcg/views/LCG_90a/${platform}
 #
-#source ${view}/setup.sh
+source ${view}/setup.sh
 #export BOOST_ROOT="${view}"
 #export DD4hep_DIR="${view}"
 #export EIGEN_INCLUDE_DIR="${view}/include/eigen3"
@@ -24,14 +24,14 @@ export LD_LIBRARY_PATH="${FASERTRACKER_DIR}/faserTracker_run/lib64:$LD_LIBRARY_P
 
 # NB: The version of the default `cmake` with the above lxplus setup is too low.
 # Directory containing cmake executable
-export PATH="$HOME/software/cmake-3.11.0-install/bin:$PATH"
+#export PATH="$HOME/software/cmake-3.11.0-install/bin:$PATH"
 
 
 # ROOT
 echo "Configuring ROOT..."
-export ROOTSYS="$HOME/software/root"
-export LD_LIBRARY_PATH="$ROOTSYS/lib:$LD_LIBRARY_PATH:$ROOTSYS"
-export PATH="$ROOTSYS/bin:$PATH"
+#export ROOTSYS="$HOME/software/root"
+#export LD_LIBRARY_PATH="$ROOTSYS/lib:$LD_LIBRARY_PATH:$ROOTSYS"
+#export PATH="$ROOTSYS/bin:$PATH"
 
 
 # End of location-specific settings
